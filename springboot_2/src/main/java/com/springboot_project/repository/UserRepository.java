9e60c193348ddcac1fa1d113187571d55c0a122f
Suraj Jha <134250774+Suraj-0828@users.noreply.github.com>
2024-09-12 03:27:08 +0530

    Delete springboot_2 directory

--- springboot_2/src/main/java/com/springboot_project/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot_project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot_project.entity.UserData;
-
-public interface UserRepository extends JpaRepository<UserData, Integer> {
-
-}
